faeeb3ce877b8bfd65f1c4934ecb2c8a2fc7dcfa
Alisina Haidari <alisinahaidari1383@gmail.com>
2024-10-03 08:57:45 +0430

    bottom nav bar added to the home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_widget/widgets/bottom_bar_widget.dart';
 import 'package:task_widget/widgets/dashboard_widget.dart';
 import 'package:task_widget/widgets/dialog_widget.dart';
 import 'package:task_widget/widgets/drawer_widget.dart';
@@ -21,6 +22,7 @@ class HomeScreen extends StatelessWidget {
           ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const DialogWidget(),));}, child: const Text('Dialog')),
           ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const DrawerWidget(),));}, child: const Text('Drawer')),
           ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const MyExpansionPanelList(),));}, child: const Text('Expansion list')),
+          ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const BottomBarWidget(),));}, child: const Text('Bottom Bar')),
           ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const TabBarWidget(),));}, child: const Text('Tab bar')),
           ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const ImageViewerWidget(),));}, child: const Text('Image viewer')),
         ],
